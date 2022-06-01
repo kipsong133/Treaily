981aa571baa702b553e6b027ffc8de825be28234
KimWooSung <kipsong133@gmail.com>
2022-06-01 18:16:51 +0900

    Feat: Google Map SDK 추가

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,15 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        GMSServices.provideAPIKey("AIzaSyAn3YelCB9FpiQ9bJCe2w-cQrS854eWCww")
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
--- lib/presentation/treasure_map/treasure_map_view.dart
+++ lib/presentation/treasure_map/treasure_map_view.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TreasureMapView extends StatefulWidget {
   const TreasureMapView({Key? key}) : super(key: key);
@@ -8,6 +10,8 @@ class TreasureMapView extends StatefulWidget {
 }
 
 class _TreasureMapViewState extends State<TreasureMapView> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: Text('MapView'),),);
